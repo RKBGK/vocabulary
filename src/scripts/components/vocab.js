f8732ec02a2a28e05b840e657eca470fc64fad27
RGKBH <rubiblr@gmail.com>
2021-09-11 13:27:47 -0500

    working search

--- src/scripts/components/vocab.js
+++ src/scripts/components/vocab.js
@@ -1,30 +1,20 @@
 import clearDom from '../helpers/data/clearDom';
 
-const showVocabcardsold = (array) => {
-  clearDom();
-  document.querySelector('#add-button').innerHTML = '';
-  document.querySelector('#add-button').innerHTML += '<button class="btn btn-success btn-lg mb-4" id="add-vocab-btn">Add A Card</button>  <button class="btn btn-success btn-lg mb-4" id="add-tech-btn">Add A Technology</button>';
-  array.forEach((item) => {
-    console.warn(item.title);
-    document.querySelector('#techstore').innerHTML += `
-      <div class="card">
-          <div class="card-body" style="height: 180px;">
-          <p class="card-title">${item.definition}</p>
-          <h5 class="card-title">${item.lang}</h5>
-
-            <hr>
-            <i class="btn btn-success fas fa-eye" id="view-voca-btn--${item.firebaseKey}"></i>
-            <i id="edit-vocab-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-            <i id="delete-vocab-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
-        </div>
-      </div>`;
-  });
-};
-
 const showVocabcards = (array) => {
   clearDom();
   document.querySelector('#add-button').innerHTML = '';
   document.querySelector('#add-button').innerHTML += '<button class="btn btn-success btn-lg mb-4" id="add-vocab-btn">Add A Card</button>  <button class="btn btn-success btn-lg mb-4" id="add-tech-btn">Add A Technology</button>';
+  document.querySelector('#add-button').innerHTML += `<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Alphabetically/a></li>
+    <li><a class="dropdown-item" href="#">Oldest</a></li>
+    <li><a class="dropdown-item" href="#">Newwest</a></li>
+  </ul>
+</div>`;
+
   array.forEach((item) => {
     console.warn(item.title);
     document.querySelector('#techstore').innerHTML += `
@@ -46,4 +36,4 @@ const emptyCards = () => {
   document.querySelector('#techstore').innerHTML = '<h1>No Items</h1>';
 };
 
-export { showVocabcards, emptyCards, showVocabcardsold };
+export { showVocabcards, emptyCards };
--- src/scripts/events/navigationEvents.js
+++ src/scripts/events/navigationEvents.js
@@ -8,6 +8,26 @@ const navigationEvents = () => {
   document.querySelector('#all-cards').addEventListener('click', () => {
     getCards().then((cardArray) => showVocabcards(cardArray));
   });
+
+  // SEARCH
+  document.querySelector('#search').addEventListener('keyup', (e) => {
+    const searchValue = document.querySelector('#search').value.toLowerCase();
+    console.warn(searchValue);
+
+    // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
+    if (e.keyCode === 13) {
+      // MAKE A CALL TO THE API TO FILTER ON THE BOOKS
+      // IF THE SEARCH DOESN'T RETURN ANYTHING, SHOW THE EMPTY STORE
+      // OTHERWISE SHOW THE STORE
+
+      document.querySelector('#search').value = '';
+      getCards()
+        .then((vCards) => {
+          const filteredcards = vCards.filter((vocab) => vocab.definition.includes(searchValue) || vocab.title.includes(searchValue));
+          showVocabcards(filteredcards);
+        });
+    }
+  });
 };
 
 // const navigationEvents = () => {
